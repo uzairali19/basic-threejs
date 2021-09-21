25d050b0ed406a2731d9cbb35b733fedd0deb2da
Uzair <uzairali19b@gmail.com>
2021-09-22 02:46:09 +0500

    fix linter error

--- src/script.js
+++ src/script.js
@@ -58,7 +58,12 @@ window.addEventListener('resize', () => {
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100,
+);
 camera.position.x = 0;
 camera.position.y = 0;
 camera.position.z = 2;
